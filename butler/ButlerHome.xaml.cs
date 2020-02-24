d2caf184891890279bf40cf0d2551d4f944349ee
Nikita Tchayka <nick@booster.cloud>
2020-02-24 22:24:13 +0000

    Add project files

---
+++ butler/ButlerHome.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace butler
+{
+    /// <summary>
+    /// Lógica de interacción para ButlerHome.xaml
+    /// </summary>
+    public partial class ButlerHome : Page
+    {
+        public ButlerHome()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var butlerReport = new ButlerReport(this.peopleListBox.SelectedItem);
+            this.NavigationService.Navigate(butlerReport);
+        }
+    }
+}
