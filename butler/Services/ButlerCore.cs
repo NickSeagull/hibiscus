b1edbd15947b6e07d1ccbad2a4a74c903f2c246e
Nikita Tchayka <nick@booster.cloud>
2020-03-02 09:07:21 +0000

    Implement basic hook example

--- butler/Services/ButlerCore.cs
+++ butler/Services/ButlerCore.cs
@@ -1,12 +1,32 @@
-﻿namespace butler.Services
+﻿using butler.LuaHelpers;
+using System;
+using System.Windows.Forms;
+
+namespace butler.Services
 {
     class ButlerCore
     {
+        KeyboardHook hook = new KeyboardHook();
+
         public void Run()
         {
             var prefs = Preferences.Load();
+
+            // TODO: Pass `KeyboardHook` reference to LuaEngine global
             var luaEngine = new LuaEngine(prefs);
             luaEngine.RunInit();
+
+            // register the event that is fired after the key press.
+            hook.KeyPressed +=
+                new EventHandler<KeyPressedEventArgs>(hook_KeyPressed);
+            // register the control + alt + F12 combination as hot key.
+            hook.RegisterHotKey(ModifierKeys.Control | ModifierKeys.Alt,
+                Keys.F12);
+        }
+
+        private void hook_KeyPressed(object sender, KeyPressedEventArgs e)
+        {
+            MessageBox.Show("pressed");
         }
     }
 }
